f38574e063a7b88e81efe80857c469a0d912f1f8
Constantine Gippius <konstantin.gippius@t-systems.com>
2021-01-28 20:52:09 +0300

    feat: minifying css

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: { importLoaders: 1 }
-                    }
+                    },
+                    'postcss-loader'
                 ]
             },
             {
